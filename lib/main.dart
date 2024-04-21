51e0097f9118562f81058324de79d7d9999140ef
AasthaPersonalGithub <aasthas2022@gmail.com>
2024-04-20 23:57:07 -0400

    Login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
-// main.dart
-
 import 'package:flutter/material.dart';
-import 'bloc/movie_bloc.dart';
-import 'utils/dark_or_light_theme.dart';
-import 'screens/main_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'firebase_options.dart';
+import 'services/auth_service.dart';
+import 'bloc/movie_bloc.dart';
+import 'screens/main_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/profile_edit_screen.dart';
+import 'utils/dark_or_light_theme.dart';
 import 'utils/movie_api.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MovieApp());
 }
 
 class MovieApp extends StatelessWidget {
   final MovieApi movieApi = MovieApi();
+  final AuthService authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
@@ -25,13 +33,33 @@ class MovieApp extends StatelessWidget {
           create: (context) => MovieBloc(movieApi: movieApi),
         ),
       ],
-      child: BlocBuilder<AppStateCubit, AppState>(
-        builder: (context, state) {
-          return MaterialApp(
-            title: 'Movie List App',
-            theme: state.themeData,
-            home: MovieListScreen(),
-          );
+      child: MaterialApp(
+        title: 'Movie List App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: StreamBuilder(
+          stream: authService.userChanges,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              // Check for the logged-in user
+              if (snapshot.hasData) {
+                return MovieListScreen();
+              }
+              return LoginScreen();
+            }
+            return Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          },
+        ),
+        routes: {
+          '/main': (context) => MovieListScreen(),
+          '/login': (context) => LoginScreen(),
+          '/profile_edit': (context) => ProfileEditScreen(),
         },
       ),
     );
